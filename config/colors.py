06b071b0c88ff4749ade5a2dad6ee38c56d6a09b
Jay <152004102+jayvsc@users.noreply.github.com>
2023-11-29 14:23:35 +0100

    Create colors.py

---
+++ config/colors.py
@@ -0,0 +1,7 @@
+finger_colors = [
+    (255, 0, 0),   # Thumb
+    (0, 255, 0),   # Index finger
+    (0, 0, 255),   # Middle finger
+    (255, 255, 0), # Ring finger
+    (255, 0, 255)  # Little finger
+]
